ba30903b1a429f9631be0cfdf9d2a90d89f66ad9
Karthik-Ragunath <a.karthikragunath@gmail.com>
2025-05-24 07:33:31 +0000

    added code to create conversations

--- create_conversations.py
+++ create_conversations.py
@@ -3,27 +3,30 @@ import requests
 url = "https://tavusapi.com/v2/conversations"
 
 payload = {
-    "replica_id": "r79e1c033f",
-    "persona_id": "p5317866",
-    "callback_url": "https://yourwebsite.com/webhook",
-    "conversation_name": "A Meeting with Hassaan",
-    "conversational_context": "You are about to talk to Hassaan, one of the cofounders of Tavus. He loves to talk about AI, startups, and racing cars.",
-    "custom_greeting": "Hey there Hassaan, long time no see!",
+    "replica_id": "rb17cf590e15",
+    # "conversation_name": "A Meeting with Hassaan",
+    # "conversational_context": "You are about to talk to Hassaan, one of the cofounders of Tavus. He loves to talk about AI, startups, and racing cars.",
+    "conversation_name": "A Meeting with Karthik",
+    "conversational_context": "You are about to talk to Karthik, one of the researchers of Tavus. He loves to talk about AI, startups, and racing cars.",
+    "custom_greeting": "Hey there Karthik, long time no see!",
     "properties": {
-        "max_call_duration": 3600,
-        "participant_left_timeout": 60,
-        "participant_absent_timeout": 300,
-        "enable_recording": True,
-        "enable_closed_captions": True,
-        "apply_greenscreen": True,
-        "language": "english",
-        "recording_s3_bucket_name": "conversation-recordings",
-        "recording_s3_bucket_region": "us-east-1",
-        "aws_assume_role_arn": ""
+        "language": "spanish"
     }
+    # "properties": {
+    #     "max_call_duration": 3600,
+    #     "participant_left_timeout": 60,
+    #     "participant_absent_timeout": 300,
+    #     "enable_recording": True,
+    #     "enable_closed_captions": True,
+    #     "apply_greenscreen": True,
+    #     "language": "spanish",
+    #     "recording_s3_bucket_name": "conversation-recordings",
+    #     "recording_s3_bucket_region": "us-east-1",
+    #     "aws_assume_role_arn": ""
+    # }
 }
 headers = {
-    "x-api-key": "<api-key>",
+    "x-api-key": "94a3b175900048ffbd42d903a83c42ca",
     "Content-Type": "application/json"
 }
 
